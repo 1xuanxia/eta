f57e54b61d121f07fb150a6e45e5eda17d0aed0b
1xuanxia <2968709717@qq.com>
2022-08-12 16:57:54 +0800

    不知道可不可以qwq

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+const headerEl = document.querySelector("header");
+const scrollToTop = document.querySelector(".scrollToTop");
+
+window.addEventListener("scroll",() => {
+  let height =  headerEl.getBoundingClientRect().height;
+  
+  if(window.pageYOffset - height > 800){
+    if(!headerEl.classList.contains("sticky")){
+    headerEl.classList.add("sticky");
+    } 
+  }
+  else{
+    headerEl.classList.remove("sticky");
+  }
+  if(window.pageYOffset >2000){
+    scrollToTop.style.display = "block";
+  } else {
+    scrollToTop.style.display="none";
+  }
+}
+)
+
 const glide = new Glide(".glide");
 const captionsEl = document.querySelectorAll(".slide-caption");
 
@@ -41,4 +63,42 @@ filterBtns.addEventListener("click", e => {
     // 筛选
     isotope.arrange({ filter: filterOption });
   }
+});
+
+//滚动动画 
+const staggeringOption = {
+  delay:300,
+  distance:"50px",
+  duration:500,
+  easing:"ease-in-out",
+  origin:"bottom"
+};
+
+ScrollReveal().reveal(".feature",{... staggeringOption,interval:350});
+ScrollReveal().reveal(".service-item",{... staggeringOption,interval:350});
+
+// 折叠按钮
+const burgerEl = document.querySelector(".burger");
+const nav = document.querySelector("header nav");
+burgerEl.addEventListener("click", () => {
+  headerEl.classList.toggle("open");
+});
+
+const scroll = new SmoothScroll('nav a[href*="#"], .scrollToTop a[href*="#"]',{
+  header:  "header",
+  offset:80
+})
+
+document.addEventListener("scrollStart",() => {
+  if(headerEl.classList.contains("open")){
+    headerEl.classList.remove("open");
+  }
+})
+
+const exploreBtnEls = document.querySelectorAll(".explore-btn");
+
+exploreBtnEls.forEach(exploreBtnEl => {
+  exploreBtnEl.addEventListener("click",() => {
+    scroll.animateScroll(document.querySelector("#about-us"));
+  });
 });
